bf679f4df6272d60f1581d71e39a4f01bc20e982
João Vitor <jvitordiass@outlook.com>
2024-02-27 17:12:04 -0300

    chore: first commit using diasjoaovitor/node-boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+export default {
+  preset: 'ts-jest',
+  testRegex: '((\\.|/*.)(test))\\.ts?$',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
+  coverageDirectory: 'coverage'
+}
