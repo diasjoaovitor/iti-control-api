04f4b18611771b5d3e537b7da6c71a59367177e0
João Vitor <jvitordiass@outlook.com>
2024-03-05 17:22:13 -0300

    feat: app server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-import dotenv from 'dotenv'
-import { sum } from './utils'
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
 
-dotenv.config()
+const PORT = process.env.PORT || 3001
 
-console.log(sum(1, 2))
-console.log(process.env.PORT)
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`> Running on http://localhost:${PORT}`)
+})
